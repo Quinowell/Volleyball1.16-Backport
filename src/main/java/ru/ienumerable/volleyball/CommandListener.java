630f93798dbc9cd70715ef4917c03df6d000756a
Sanya <54205453+Quinowell@users.noreply.github.com>
2023-08-07 21:47:22 +0300

    В 11 джаве так нельзя

--- src/main/java/ru/ienumerable/volleyball/CommandListener.java
+++ src/main/java/ru/ienumerable/volleyball/CommandListener.java
@@ -14,27 +14,23 @@ public class CommandListener implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        switch (command.getLabel()) {
-            case "ballskin" -> {
-                if (sender instanceof Player player) {
-
-                    ItemStack item = player.getInventory().getItemInMainHand();
+        if (command.getName().equalsIgnoreCase("ballskin")) {
+                    ItemStack item = Bukkit.getPlayer(sender.getName()).getInventory().getItemInMainHand();
 
                     if (!item.hasItemMeta() || !SkullSkin.isContainSkin(item)) {
-                        player.sendMessage(ChatColor.RED + "Возьмите мяч в руки, чтобы сменить скин!");
+                        Bukkit.getPlayer(sender.getName()).sendMessage(ChatColor.RED + "Возьмите мяч в руки, чтобы сменить скин!");
                         return true;
                     }
 
-                    SkinChanger skinChanger = new SkinChanger(player, SkullSkin.getSkin(item));
+                    SkinChanger skinChanger = new SkinChanger(Bukkit.getPlayer(sender.getName()), SkullSkin.getSkin(item));
                     skinChanger.openMenu();
-                }
+                
             }
-            case "ballsreload" -> {
+        if (command.getName().equalsIgnoreCase("ballreload")){
                 Volleyball.getBallsContainer().dropAllBalls();
                 Volleyball.loadConfig();
                 sendMsgToOps(ChatColor.GREEN + "Презагружено!");
             }
-        }
 
         return true;
     }
